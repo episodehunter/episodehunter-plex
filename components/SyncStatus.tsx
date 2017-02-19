19edd32f04fc33f79a41a80b7b708477a8cda408
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-02-19 17:08:55 +0100

    💄  Set error color

--- components/SyncStatus.tsx
+++ components/SyncStatus.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 export function SyncStatus(props: {error: string}) {
   if (props.error) {
-    return <h1>{props.error}</h1>;
+    return <h1 style={{color: '#ff5f5f', fontSize: '16px'}}>{props.error}</h1>;
   } else {
     return <h1>Redy to sync</h1>;
   }
