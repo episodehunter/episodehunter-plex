054299c02d8f5df28f133decedb0847847c3e331
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-07-16 06:07:58 +0200

    ✨ Include raven

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,13 +1,32 @@
 const electron = require('electron');
+import * as Raven from 'raven-js';
 
 export function createLogger() {
+    Raven
+        .config('https://14a889bd545a496a8de6066cf18146de@sentry.io/192024')
+        .install();
     const remote = electron.remote;
     if (remote) {
         const log = remote.require('electron-log');
         log.transports.console.level = 'info';
         log.transports.file.level = 'info';
-        return log;
+        return {
+            info: msg => {
+                log.info(msg);
+            },
+            error: msg => {
+                log.error(msg);
+                Raven.captureMessage(msg);
+            }
+        };
     } else {
-        return {};
+        return {
+            info: msg => {
+                Raven.captureMessage(msg);
+            },
+            error: msg => {
+                Raven.captureMessage(msg);
+            }
+        };
     }
 }
